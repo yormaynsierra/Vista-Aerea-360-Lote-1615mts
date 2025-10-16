74ee756b05a5cda7709a9c78100c46862aa40534
Yormayn Sierra Inmobiliaria <yorsierra9@gmail.com>
2025-10-15 19:05:12 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vista-360-lote-1615",
+      "name": "Vista 360 lote 1615",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.962551917731018,
+          "pitch": 0.7288409448761595,
+          "title": "Dato de Interes.",
+          "text": "Casa Modular de 71mt2."
+        },
+        {
+          "yaw": 2.0879532380638954,
+          "pitch": 0.8475946460634844,
+          "title": "Dato de Interes.&nbsp;",
+          "text": "Zona de alta valorización.&nbsp;<div>Ubicado en Riofrio Oriental - Tabio</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Vista Aérea  Lote 1615mts",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
